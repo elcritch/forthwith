f878c795fbf3acd81e331bce348d09159af72d0a
Jaremy Creechley <creechley@gmail.com>
2018-08-08 12:07:10 -0600

    Various cleanups, docs

--- src/forthwith-linux.h
+++ src/forthwith-linux.h
@@ -10,10 +10,14 @@
 #define $word_max $0xFFFFFFFFFFFFFFFF
 #define $word_ptr_sz $8
 
-/* %rdi %rsi %rdx %rcx %r8 %r9 %r10 */
-/* gdb> info register rdi rsi rdx rcx r8 r9 r10 */
-/* lldb> register read rdi rsi rdx rcx r8 r9 r10 */
 
+// ========================================================================== //
+// Platform Registers 
+// ========================================================================== //
+
+/* The C macros below expand registers from 'w' to 'reg_ ## w' which then
+ get substituted with whats here. This helps with string concat of the various
+ registers. */
 #define reg_w   %rdi
 #define reg_tos %rsi
 #define reg_x   %rdx
@@ -26,8 +30,19 @@
 #define reg_c %r13
 #define reg_rax %rax
 #define reg_rip %rip
+/* %rdi %rsi %rdx %rcx %r8 %r9 %r10 */
+/* gdb> info register rdi rsi rdx rcx r8 r9 r10 */
+/* lldb> register read rdi rsi rdx rcx r8 r9 r10 */
 
-#define fw_label(l) __asm__("" #l ":")
+
+// ========================================================================== //
+// Platform "Helpers"
+// ========================================================================== //
+
+/*
+  e.g. macro hackery. pretty sure there's some incantations to Cthululu somewhere in here... but not too much worse (IMHO) compared to asm macro's, much less 20 different asm macro syntaxes
+*/
+
 /* #define fw_label(l) _fw_label(l) */
 #define _asm_jump() \
   __asm__ ("" :: "r" (w));          \
@@ -36,7 +51,7 @@
   __asm__ ("" :: "r" (rsp));        \
   __asm__ ("" :: "r" (tos))
 
-/* #define __jump(reg) asm("jmp "##reg) */
+// Define some specific jumps, by linux, this should support most unix-likes or proper unixes
 #ifdef __MACH__
 #define __jump(r) __asm__("jmp " "_" #r)
 #elseif __linux__
@@ -45,19 +60,11 @@
 #define __jump(r) __asm__("jmp " #r)
 #endif
 
-/* #define _jump(r) __jump( _ ## r) */
 #define _jump(r) __jump( r )
-#define jump(reg) _jump( reg ); _asm_jump()
-
-/* #define jump(reg) goto *((fcell_t*)reg); */
-
-#define ___jump_reg(r)                        \
-  __asm__(r); 
-  /* _asm_jump() */
+#define ___jump_reg(r) __asm__(r); 
 
 #define __jump_reg(r) ___jump_reg( "jmpq *" #r )
 #define _jump_reg(r, x) __jump_reg( r )
-#define jump_reg(r) _jump_reg( reg_ ## r, __jump_reg )
 
 #define _fw_asm(r, a, x, b, c, y, d) __asm__(r " " a #x b "," c #y d)
 
@@ -66,6 +73,17 @@
 #define _fw_asm_const(c, x, y) _fw_asm(c, "", x, "", "", y, "")
 #define _fw_asm_to_addr_off(c, x, y, o) _fw_asm(c, "", x, "", o "(", y, ")")
 #define _fw_asm_from_addr_off(c, x, y, o) _fw_asm(c, o "(", x, ")", "", y, "")
+
+// ========================================================================== //
+// Standard "Definitions"
+// ========================================================================== //
+
+/*
+  The forth impl interfaces 
+ */
+
+#define jump_reg(r) _jump_reg( reg_ ## r, __jump_reg )
+#define jump(reg) _jump( reg ); _asm_jump()
 
 #define load_const(x, y) _fw_asm_const("movq", y, reg_##x)
 #define load_addr(x, y) _fw_asm_from_addr("movq", reg_##y, reg_##x)
@@ -98,10 +116,11 @@ extern struct forthwith_context *ctx;
 #define _pushu(reg) store_addr(u, reg); add_const(u, $word_sz)
 #define _popu(reg)  sub_const(u, $word_sz); load_addr(reg, u)
 
+#define $fw_ctx_offset_psp_header "8"
+
 #define save_psp(reg) \
   load_addr_off(rax, rip, "ctx"); \ 
-  store_addr_off(rax, reg, "8" ) // sizeof one word
-  /* store_addr_off(reg, rax, offsetof(fw_ctx_t, psp_head) ) */
+  store_addr_off(rax, reg, $fw_ctx_offset_psp_header) // sizeof one word
 
 #endif // __HEADER_IMPL_X86__
 
