ba028f3f80e37b581a1491fba6646fb36b4b0972
Jaremy Creechley <creechley@gmail.com>
2018-09-02 00:35:19 -0600

    fix linux-arm

--- src/linux-arm/forthwith-main.c
+++ src/linux-arm/forthwith-main.c
@@ -1,22 +1,30 @@
 
+#define FW_CUSTOM_TOB_FLUSH
+
 #include "forthwith-linux.h"
 #include "forthwith.h"
 #include "dict.h"
 #include "prompt.h"
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
 
   forth_init();
   forth_bootstrap();
 
-  char *rx_buff[128];
-  char *tx_buff[128];
+  const int buff_sz = 1024;
+  char *rx_buff = calloc(1, buff_sz);
+  char *tx_buff = calloc(1, buff_sz);
 
-  prompt_init();
+  prompt_init((char*)rx_buff, buff_sz, (char*)tx_buff, buff_sz);
+
+  int prompt_read = 1;
   for (;;) {
-    prompt_do((char*)rx_buff + 0, 128, (char*)tx_buff, 128);
-  } 
+    prompt_read = prompt_do(prompt_read);
+  }
 
 }
+
