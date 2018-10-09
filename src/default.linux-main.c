e55e5b93ce9bb9771fa189f95f042c565c7c0c95
Jaremy Creechley <creechley@gmail.com>
2018-10-08 20:07:21 -0600

    fix docs

--- src/default.linux-main.c
+++ src/default.linux-main.c
@@ -9,12 +9,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern struct user_ptrs_array user_ptrs;
+#define USER_PTR_CNT 128
+
+user_ptr_t __user_ptrs[USER_PTR_CNT];
+
 int main(int argc, char** argv) {
   struct forth_init_sizes init_sizes = { .psp = 512,
                                          .rsp = 512,
                                          .user = 16384,
                                          .dict = 16384,
                                          .strings = 16384 };
+
+  user_ptrs.ptrs = &__user_ptrs[0];
+  user_ptrs.count = USER_PTR_CNT;
 
   forth_init(init_sizes);
   forth_bootstrap();
