26761d71ea6c1f4f71575a34fe9259de00b33b51
Jaremy Creechley <creechley@gmail.com>
2018-10-09 17:36:12 -0600

    adding newlines to core lib -- need to figure out missing words

--- library/update-core.sh
+++ library/update-core.sh
@@ -3,5 +3,5 @@
 cat library/core.fth | \
   grep -v '^$' | \
   grep -v '^\\' | \
-  perl -pe 'BEGIN{ print "\n#include \"core-lib.h\"\n\nconst char *fw_core_lib[MAX_CORE_LIBS] = {\n"; $i = 0; }; s/^(.+)$/    "\1",/; $i++; END{ print "\n}; "; }' > src/core-lib.c
+  perl -pe 'BEGIN{ print "\n#include \"core-lib.h\"\n\nconst char *fw_core_lib[MAX_CORE_LIBS] = {\n"; $i = 0; }; s/;/;\\n/; s/^(.+)$/    "\1",/; $i++; END{ print "\n}; "; }' > src/core-lib.c
 
